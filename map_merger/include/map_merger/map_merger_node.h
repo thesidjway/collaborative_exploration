7caf4ff7ae79e939b9c336b122ef7711a61ad32e
Siddharth Jha <siddharthjha@outlook.com>
2019-09-02 19:21:50 +0530

    Basic map merger almost working, needs some bug fixing

--- map_merger/include/map_merger/map_merger_node.h
+++ map_merger/include/map_merger/map_merger_node.h
@@ -1,12 +1,31 @@
 #pragma once
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <geometry_msgs/Point.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <std_msgs/ColorRGBA.h>
+#include <vector>
+#include <utility>
 
 ros::Subscriber octo_sub;
 ros::Subscriber map_sub;
-ros::Publisher merged_octo_pub;
-int map_size = 1000;
-double map_res = 0.0500000007451;
+ros::Publisher occupied_pub;
+const double map_res = 0.05;
+const double octo_res = 0.4;
+const int width = 125;
+const int width_map = 1000;
+const int height = 80;
+const int height_map = 640;
+const double x_orig = -25.025;
+const double y_orig = -16.025;
+
+bool received_octomap = false;
+int grid_2d[width][height];
+visualization_msgs::MarkerArray last_octomap;
+std::map<std::pair<int, int>, int> explored;
+
+void publish_updated(std::map<std::pair<int, int>, int>& explored_to_be_updated);
+void map_callback(const nav_msgs::OccupancyGrid::ConstPtr& map);
+void octo_callback(const visualization_msgs::MarkerArray::ConstPtr& octomap);
 
 
--- map_merger/src/map_merger_node.cpp
+++ map_merger/src/map_merger_node.cpp
@@ -2,22 +2,49 @@
 #include <map_merger/MapMerger.h>
 
 void octo_callback(const visualization_msgs::MarkerArray::ConstPtr& octomap) {
-	for (auto i : octomap->markers) {
-		for (auto j: i.points) {
-			//std::cout << j << std::endl;
-		}
-	}
+	received_octomap = true;
+	last_octomap = *octomap;
 }
 
 void map_callback(const nav_msgs::OccupancyGrid::ConstPtr& map) {
-	for(int i = 0; i < 1000; i++) {
-		for (int j = 0 ; j < 1000 ; j++) {
-			int dat = map->data[i *1000 + j];
+	for(int i = 0; i < width_map; i++) {
+		for (int j = 0 ; j < height_map ; j++) {
+			int dat = map->data[i * height_map + j];
 			if (dat >= 0) {
-				//std::cout << double(i)*map_res << ", " << double(j)*map_res << ": " << dat << std::endl;
+				grid_2d[int(i/8)][int(j/8)] = dat;
+				std::pair<int, int> new_cell = std::make_pair<int, int> (int(i/8), int(j/8));
+				if (dat == 0) {
+					explored[new_cell] = 0;
+				} else if (dat == 100) {
+					explored[new_cell] = 100;
+				}
 			}
 		}
 	}
+	if (received_octomap) {
+		publish_updated(explored);
+	}
+}
+
+void publish_updated(std::map<std::pair<int, int>, int>&  explored) {
+	std::cout << "Size explored: " << explored.size() << std::endl;
+	std_msgs::ColorRGBA clr;
+	clr.r = 1;
+	clr.g = 1;
+	clr.b = 0;
+	clr.a = 1;
+	for (auto i : explored) {
+		if (i.second == 0) {
+			geometry_msgs::Point pt;
+			pt.x = i.first.first * octo_res + x_orig;
+			pt.y = i.first.second * octo_res + y_orig;
+			pt.z = 0.2;
+			last_octomap.markers[last_octomap.markers.size() - 1].points.push_back(pt);
+			last_octomap.markers[last_octomap.markers.size() - 1].colors.push_back(clr);
+		}
+	}
+	occupied_pub.publish(last_octomap);
+	received_octomap = false;
 }
 
 int main(int argc, char** argv) {
@@ -26,5 +53,6 @@ int main(int argc, char** argv) {
 	MapMerger mm;
 	octo_sub = nh.subscribe("/husky/map", 1, map_callback);
 	map_sub = nh.subscribe("/baroness/nbvPlanner/octomap_occupied", 1, octo_callback);
+	occupied_pub = nh.advertise<visualization_msgs::MarkerArray>("/baroness/nbvPlanner/octomap_occupied_merged", 1000);
 	ros::spin();
 }
