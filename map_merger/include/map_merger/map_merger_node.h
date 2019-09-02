375fcaf418764bb8d4670e23463065ad275f9971
Siddharth Jha <siddharthjha@outlook.com>
2019-09-02 14:41:10 +0530

    Collected data for merging

--- map_merger/include/map_merger/map_merger_node.h
+++ map_merger/include/map_merger/map_merger_node.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <ros/ros.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <visualization_msgs/MarkerArray.h>
+
+ros::Subscriber octo_sub;
+ros::Subscriber map_sub;
+ros::Publisher merged_octo_pub;
+int map_size = 1000;
+double map_res = 0.0500000007451;
+
+
--- map_merger/src/map_merger_node.cpp
+++ map_merger/src/map_merger_node.cpp
@@ -1,7 +1,30 @@
 #include <map_merger/map_merger_node.h>
 #include <map_merger/MapMerger.h>
 
-int main(int argc, char** argv) {
-	MapMerger mm;
+void octo_callback(const visualization_msgs::MarkerArray::ConstPtr& octomap) {
+	for (auto i : octomap->markers) {
+		for (auto j: i.points) {
+			//std::cout << j << std::endl;
+		}
+	}
+}
 
+void map_callback(const nav_msgs::OccupancyGrid::ConstPtr& map) {
+	for(int i = 0; i < 1000; i++) {
+		for (int j = 0 ; j < 1000 ; j++) {
+			int dat = map->data[i *1000 + j];
+			if (dat >= 0) {
+				//std::cout << double(i)*map_res << ", " << double(j)*map_res << ": " << dat << std::endl;
+			}
+		}
+	}
+}
+
+int main(int argc, char** argv) {
+	ros::init(argc, argv, "map_merger");
+	ros::NodeHandle nh; 
+	MapMerger mm;
+	octo_sub = nh.subscribe("/husky/map", 1, map_callback);
+	map_sub = nh.subscribe("/baroness/nbvPlanner/octomap_occupied", 1, octo_callback);
+	ros::spin();
 }
